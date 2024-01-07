c3b21b9ff61d54a626d2eed1ffa9da735c3eb656
IgoRand9 <123418246+IgoRand9@users.noreply.github.com>
2024-01-07 22:10:20 +0300

    minor changes

--- Game/Mobs/zombie.gd
+++ Game/Mobs/zombie.gd
@@ -41,8 +41,18 @@ func _on_detector_body_exited(body):
 func _on_death_body_entered(body):
 	if body.name == "Player":
 		death()
+		
+func _on_death_2_body_entered(body):
+	if body.name == "Player":
+		if alive == true:
+			body.health -= 40 
+		death()
+	
 func death ():
 	alive = false 
 	anim.play("Death")
 	await anim.animation_finished
 	queue_free()
+
+
+
--- Game/Player/player.gd
+++ Game/Player/player.gd
@@ -1,5 +1,14 @@
 extends CharacterBody2D
 
+enum  {            # стейтмашин отвечает за анимации
+	MOVE,
+	ATTACK,
+	ATTACK2,
+	ATTACK3,
+	BLOCK,
+	JUMP 
+}
+	
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -250.0
@@ -8,37 +17,62 @@ const JUMP_VELOCITY = -250.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var anim = $AnimatedSprite2D
-
-
+@onready var animPlayer = $AnimationPlayer
+var health = 100
+var state = MOVE
 
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state()
+		ATTACK:
+			attack_state()
+		ATTACK2:
+			pass
+		ATTACK3:
+			pass
+		JUMP:
+			pass
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+		
+	if health <= 0:
+		health = 0
+		queue_free()
+		get_tree().change_scene_to_file("res://menu.tscn")
+		
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		anim.play("Jump")
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	move_and_slide()
+	
+func move_state ():                                 # функция для передвижения персонажа 
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 		if velocity.y == 0:
-			anim.play("Run")
-		
+			animPlayer.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if velocity.y == 0:
-			anim.play("Idle")
-			
+			animPlayer.play("Idle")
 	if direction == -1:
-		$AnimatedSprite2D.flip_h = true
+		anim.flip_h = true
 	
 	elif direction == 1:
-		$AnimatedSprite2D.flip_h = false
+		anim.flip_h = false
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
 		
-	move_and_slide()
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		animPlayer.play("jump")
+
+func attack_state():
+	velocity.x = 0
+	animPlayer.play("attack")
+	await  animPlayer.animation_finished
+	state = MOVE
+	
